31cb1198191b1e12f6ebce4bd908c893382ba3e5
Abdulmalek Almkainzi <aalmkainzi@gmail.com>
2024-03-31 09:38:28 +0300

    fixed weapon damage needing an instance of Enemy

--- Survival-Game/Assets/Scripts/Weapon.cs
+++ Survival-Game/Assets/Scripts/Weapon.cs
@@ -5,15 +5,12 @@ using UnityEngine;
 public class Weapon : MonoBehaviour
 {
     public float Damage;
-    public Enemy eH;
+
     public void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Enemy"))
         {
-
-            eH.eHealth -= Damage;
+            collision.gameObject.GetComponent<Enemy>().eHealth -= Damage;
         }
-
     }
-
 }
